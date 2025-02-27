f0f34597c54849baf86e75fff2d23dfb81dec865
Aaron040-Rmz <Aaron.rmz122@gmail.com>
2025-02-27 14:28:11 -0600

    add_e.p.

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-"""
-main
-"""
+from Neural_networks_keras_2.src.keras_2 import train_image_detection_model
+
+def main():
+
+    model_image = train_image_detection_model()
+    print(model_image)
+    
+   
+if __name__ == "__main__":
+    main()
